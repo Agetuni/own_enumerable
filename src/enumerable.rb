3ae90e6104d74d7f12fc8f2e14aac87f9cb92d7e
Aleazar yilma <yilma4060@gmail.com>
2022-10-27 18:39:35 +0300

    create mylist and enumerable

---
+++ src/enumerable.rb
@@ -0,0 +1,27 @@
+module MyEnumerable
+  def all?
+    @return_value = true
+    @list.each do |n|
+      @return_value = if yield n
+                        true
+                      else
+                        false
+                      end
+    end
+    @return_value
+  end
+
+  def any?
+    @return_value = false
+    @list.each { |n| @return_value = true if yield n }
+    @return_value
+  end
+
+  def filter
+    @filtered_array = []
+    @list.each do |n|
+      @filtered_array << n if yield n
+    end
+    @filtered_array
+  end
+end
